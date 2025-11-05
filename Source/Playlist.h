06452a2c9101c07d53526a4c2293ba369ac80a6e
renadsarwat7 <renadsarwat270@gmail.com>
2025-11-05 18:22:32 +0200

    Add Playlist class with file management methods

--- Source/Playlist.h
+++ Source/Playlist.h
@@ -1,1 +1,36 @@
+#pragma once
+#include <JuceHeader.h>
+#include "MetadataReader.h"
 
+
+
+class Playlist
+{
+public:
+    Playlist();
+
+    void addFile(const juce::File& file);
+    void addFiles(const juce::Array<juce::File>& files);
+    void removeFile(int index);
+    void clear();
+
+    int getNumFiles() const;
+    juce::String getFileName(int index) const;
+    juce::File getFile(int index) const;
+
+    int getCurrentIndex() const;
+    void setCurrentIndex(int newIndex);
+    bool hasNext() const;
+    bool hasPrevious() const;
+    void next();
+    void previous();
+
+
+
+    juce::File getCurrentFile() const;
+
+private:
+    juce::Array<juce::File> audioFiles;
+    int currentIndex = -1;
+
+};
