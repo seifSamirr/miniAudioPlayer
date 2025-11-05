d17a623dc8a2ca466f9a73e0fd30bfcbc480abdf
renadsarwat7 <renadsarwat270@gmail.com>
2025-11-05 19:20:18 +0200

    Add AudioMetadata struct and MetadataReader class

--- Source/MetadataReader.h
+++ Source/MetadataReader.h
@@ -1,1 +1,22 @@
+#pragma once
+#include <JuceHeader.h>
 
+struct AudioMetadata
+{
+    juce::String title;
+    juce::String artist;
+    juce::String album;
+    juce::String genre;
+    int year = 0;
+    double duration = 0.0;
+    juce::String filePath;
+};
+
+class MetadataReader
+{
+public:
+    static AudioMetadata readMetadata(const juce::File& audioFile);
+
+private:
+    static juce::String getMetadataFromFormat(juce::AudioFormatReader* reader, const juce::String& key);
+};
